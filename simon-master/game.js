087c34968c8af3595db91832a1efac54400bd151
Vishakh M V <vishakhmv667@gmail.com>
2026-04-04 12:01:41 +0530

    add simon master logic

---
+++ simon-master/game.js
@@ -0,0 +1,94 @@
+gamePattern=[];
+buttonColours=["red", "blue", "green", "yellow", "purple", "orange"];
+userClickedPattern=[];
+var start=false;
+var level=0;
+var highScore = 0;
+
+
+function nextSequence(){
+    userClickedPattern=[];
+    var randomNumber = Math.floor((Math.random()*buttonColours.length));
+    var randomChosenColour=buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    shuffleButtons(); 
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    var a = new Audio("sounds/"+randomChosenColour+".mp3");
+    a.play();
+    level=level+1;
+    $("h1").text("Level "+level);
+}
+
+$(document).on("click", ".btn", function(){
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    checkAnswer(userClickedPattern.length-1);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+function playSound(name){
+        var a = new Audio("sounds/"+name+".mp3");
+        a.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+$(document).keypress(function(event){
+    if(!start)
+    {
+        nextSequence();
+        start=true;
+    }
+});
+
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel]===gamePattern[currentLevel])
+    {
+        if(userClickedPattern.length===gamePattern.length)
+        {
+              if(level > highScore){
+                highScore = level;
+                $("#highscore").text("High Score: " + highScore);
+            }
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else
+    {
+        var w = new Audio("sounds/wrong.mp3");
+        w.play();
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("#level-title").text("Game over. You lost at level  "+level+" Press any key to restart");
+        startOver();
+    }
+}
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    start=false;
+}
+
+function shuffleButtons() {
+    let container = $(".container");
+    let buttons = container.find(".btn").toArray();
+    for (let i = buttons.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [buttons[i], buttons[j]] = [buttons[j], buttons[i]];
+    }
+    container.empty();
+    buttons.forEach(button => container.append(button));
+}
