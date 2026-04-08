9980915edc7430a02047a05dfef1c12055d688ce
Vishakh M V <vishakhmv667@gmail.com>
2026-04-08 16:46:26 +0530

    Add prank feature to Drum Pulse

--- Drum-pulse/index.js
+++ Drum-pulse/index.js
@@ -20,47 +20,47 @@ document.addEventListener("keydown",function(event){
  function makeSound(key){
             switch (key) {
             case "w":
-                var tom1=new Audio("./sounds/tom-1.mp3");
+                var tom1=new Audio("./sounds/snare.mp3");
                 tom1.play();                
                 break;
 
                 case "a":
-                var tom2=new Audio("./sounds/tom-2.mp3");
+                var tom2=new Audio("./sounds/kick-bass.mp3");
                 tom2.play();                
                 break;
         
                 case "s":
-                var tom3=new Audio("./sounds/tom-3.mp3");
+                var tom3=new Audio("./sounds/tambourine.mp3");
                 tom3.play();                
                 break;
 
                 case "d":
-                var tom4=new Audio("./sounds/tom-4.mp3");
+                var tom4=new Audio("./sounds/crash.mp3");
                 tom4.play();                
                 break;
 
                 case "f":
-                var snare=new Audio("./sounds/snare.mp3");
+                var snare=new Audio("./sounds/open.mp3");
                 snare.play();                
                 break;
 
                 case "g":
-                var kickbass=new Audio("./sounds/kick-bass.mp3");
+                var kickbass=new Audio("./sounds/tom-1.mp3");
                 kickbass.play();                
                 break;
 
                 case "h":
-                var clap=new Audio("./sounds/tambourine.mp3");
+                var clap=new Audio("./sounds/tom-2.mp3");
                 clap.play();                
                 break;
 
                 case "j":
-                var crash=new Audio("./sounds/crash.mp3");
+                var crash=new Audio("./sounds/tom-2.mp3");
                 crash.play();
                 break;
 
                 case "k":
-                var open=new Audio("./sounds/open.mp3");
+                var open=new Audio("./sounds/snare.mp3");
                 open.play();                
                 break;
 
